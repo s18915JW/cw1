bee522c1d137212e9f2aa2a059efab01cf316bd0
Jakub Woźniak <s18915@pjwstk.edu.pl>
2020-03-03 09:51:19 +0100

    init commit

---
+++ cw1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			// Console.WriteLine("Hello World!");
+
+			var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl/";
+			var client = new HttpClient();
+			var res = await client.GetAsync(url);
+
+			if (res.IsSuccessStatusCode)
+			{
+				var content = await res.Content.ReadAsStringAsync();
+				var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+				var matches = regex.Matches(content);
+
+				foreach (var item in matches)
+				{
+					Console.WriteLine(item.ToString());
+				}
+			}
+		}
+	}
+}
